bd3b3b3627aca47c5700a8594509c831fee114d9
whysofail <adam.satria.armansyah@gmail.com>
2023-07-27 06:40:56 +0700

    add: style button pagination

---
+++ app/Controllers/Shahih_Muslim.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ShahihMuslimModel;
+
+class Shahih_Muslim extends BaseController
+{
+    
+    public function view()
+    {
+        // Load the ShahihBukhariModel using dependency injection
+        $model = new ShahihMuslimModel();
+
+        // Get the current page from the query string, default to 1 if not set
+        $currentPage = $this->request->getVar('page') ?? 1;
+
+        // Fetch records with pagination using the model's paginate() method
+        $musl = $model->paginate(10, 'group2'); // 10 records per page, 'group1' is the pagination group
+
+        // Get the pagination links
+        $pager = $model->pager;
+
+        $data = [
+            'musl' => $musl,
+            'pager'  => $pager,
+        ];
+        // Load the view file directly without creating a new folder
+        return view('page/shahih_muslim_view', $data);
+    }
+}
